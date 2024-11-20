a3b07eb9889aafc4a7e9a2ad86c80d26d789a8e0
arielperstin10 <arielperstin10@gmail.com>
2024-11-21 00:50:01 +0200

    Added models for project

---
+++ eBookLibrary/Data/Enums/Genre.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace eBookLibrary.Data.Enums
+{
+    public enum Genre
+    {
+        Action,
+        Adventure,
+        Fantasy,
+        ScienceFiction,
+        Mystery,
+        Horror,
+        Romance,
+        HistoricalFiction,
+        LiteraryFiction,
+        Autobiography
+    }
+}
